1009e537c2dc858e9a4d799890d6ec8714e762ea
mythicalBeast15x <117381703+mythicalBeast15x@users.noreply.github.com>
2024-04-04 00:00:36 -0400

    Please work!!!!!

--- main/ControlScreen.go
+++ main/ControlScreen.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"fmt"
 	lcd "github.com/wjessop/lcm1602_lcd"
 	"golang.org/x/exp/io/i2c"
 	"log"
+	"strings"
 )
 
-func main() {
+type LCD interface {
+	WriteLCD()
+	ClearLCD()
+}
+
+func WriteLCD(LCDMessage string) string {
 	// Configure this line with the device location and address of your device
 	lcdDevice, err := i2c.Open(&i2c.Devfs{Dev: "/dev/i2c-1"}, 0x27)
 	if err != nil {
@@ -19,17 +26,53 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// Write a string to row 1, position 0 (ie, the start of the line)
-	if err := lcdDisplay.WriteString("Hello World!", 1, 0); err != nil {
-		log.Fatal(err)
+	if len(LCDMessage) <= 16 {
+		err = lcdDisplay.WriteString(LCDMessage, 0, 0)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	} else if len(LCDMessage) <= 32 {
+		lines := strings.Split(LCDMessage, "")
+		line1 := strings.Join(lines[:16], "")
+		line2 := strings.Join(lines[16:], "")
+
+		// Write to both lines
+		err = lcdDisplay.WriteString(line1, 0, 0)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = lcdDisplay.WriteString(line2, 1, 0)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		// Message is longer than 32 characters
+		errMsg := "Message too long"
+		fmt.Println(errMsg)
+		return errMsg
 	}
 
-	// Write a string to row 2, position 7
-	if err := lcdDisplay.WriteString("(>'.'<)", 2, 7); err != nil {
+	return ""
+}
+func ClearLCD() {
+	// Configure this line with the device location and address of your device
+	lcdDevice, err := i2c.Open(&i2c.Devfs{Dev: "/dev/i2c-1"}, 0x27)
+	if err != nil {
 		log.Fatal(err)
 	}
+	defer lcdDevice.Close()
 
+	lcdDisplay, err := lcd.NewLCM1602LCD(lcdDevice)
+	if err != nil {
+		log.Fatal(err)
+	}
 	if err := lcdDisplay.Clear(); err != nil {
 		log.Fatal(err)
+
 	}
+}
+
+func main() {
+	WriteLCD("DOMATIX")
 }
